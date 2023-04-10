8558c9bea2b266e4a26a40e85f0ba6018c0a5a26
Rakesh Baruah <rbaruah@gmail.com>
2023-04-10 09:55:25 -0500

    initial comit

---
+++ sanity/config/client-config.ts
@@ -0,0 +1,9 @@
+import { createClient, groq } from "next-sanity";
+
+const config = {
+  projectId: "m46c2r4a",
+  dataset: "production",
+  apiVersion: "2023-04-09",
+};
+
+export default config;
---
+++ sanity/sanity-utils.ts
@@ -0,0 +1,35 @@
+import config from "@/sanity.config";
+import { Project } from "@/types/Project";
+import { createClient, groq } from "next-sanity";
+import clientConfig from "./config/client-config";
+
+export async function getProjects(): Promise<Project[]> {
+  return createClient(clientConfig).fetch(
+    groq`*[_type == "project"]{
+        _id,
+        _createdAt,
+        name,
+        "slug": slug.current,
+        "image": image.asset -> url,
+        alt,
+        url,
+        content
+    }`
+  );
+}
+
+export async function getProject(slug: string): Promise<Project> {
+  return createClient(clientConfig).fetch(
+    groq`*[_type == "project" && slug.current == $slug][0]{
+        _id,
+        _createdAt,
+        name,
+        "slug": slug.current,
+        "image": image.asset -> url,
+        alt,
+        url,
+        content
+    }`,
+    { slug }
+  );
+}
